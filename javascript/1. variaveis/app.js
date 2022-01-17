579c5f1d197cb77a65a0d7fc0bccbe4a085bec95
Helber <helber.edu@gmail.com>
2022-01-16 21:48:22 -0300

    first day, practicing variables

---
+++ javascript/1. variaveis/app.js
@@ -0,0 +1,27 @@
+var name = 'Helber' // string
+var age = 23 // integer
+var weight = 64.5 // float
+var alive = false // boolean
+var food = null // null
+var time // undefined
+
+var composta = ['', 23, 66.8, false, null] // variavel composta
+
+var fruits = ['maça', 'melão',] // variavel composta
+
+var person = {
+    name: name,
+    age: 23,
+    alive: true,
+    height: 1.64,
+} // object
+
+console.log(typeof person); // typeof X checa o tipo da variável de nome X.
+
+console.log(person);
+
+console.log(person.name); // Objeto.Propriedade para acessar o valor da propriedade dentro do objeto.
+
+console.log(fruits);
+
+console.log(fruits[0]); // Array[Posição] para acessar valor em determinada posição.
