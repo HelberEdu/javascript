b2447430547a3ce1f28a66029f876df637f89820
Helber <helber.edu@gmail.com>
2022-01-24 21:53:36 -0300

    third day, practicing arithmetic operators

---
+++ javascript/3. sd/app.js
@@ -0,0 +1,9 @@
+// Some + ; Subtração - ; Multiplicação * ; Divisão / ; Módulo(resto da divisão) % ; Prioriza operações usando () ;
+var x = 10
+var y = 3
+
+x++ // Incrementa a variavel x em 1, mas retorna o valor anterior
+x-- // Decrementa a variável x em 1, mas retorna o valor anterior
+var result = x % y
+
+console.log(x) // console.log(x++) mostra o valor anterior de x, console.log(++x) mostra o valor de x já incrementado.
