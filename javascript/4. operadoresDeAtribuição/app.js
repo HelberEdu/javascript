256ab2344b585693e12485a8aedaa6cafbf41742
Helber <helber.edu@gmail.com>
2022-01-25 22:39:05 -0300

    fourth day, practicing assignment operators

---
+++ javascript/4. operadoresDeAtribuição/app.js
@@ -0,0 +1,15 @@
+var x = 10
+var y = 3
+
+// result = x + y ou
+
+x += y // nesse caso, x soma com y, mas é possível realizar outras operações como -, *, / e %.
+
+console.log(x)
+
+var product = 'Camisa'
+var text = 'Temos em estoque: '
+
+text += product // concatena as strings
+
+console.log(text)
