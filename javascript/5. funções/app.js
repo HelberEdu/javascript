42035af59b78d76ccf9b62c154fc2d41556423ef
Helber <helber.edu@gmail.com>
2022-01-26 22:41:59 -0300

    fifth day, practicing functions

---
+++ javascript/5. funções/app.js
@@ -0,0 +1,13 @@
+function sum(number1, number2) {
+    return number1 + number2
+} 
+
+// {} serve para declarar um escopo(ação de alguma coisa).
+
+// 'number1' e 'number2' são chamados de parâmetros da função, mas quando a função e chamada e os valores são adicionados, esses valores se chamam 'argumentos'. Portanto o primeiro argumento irá preencher o primeiro parâmetro e assim sucessivamente.
+
+// os parâmetros são visíveis apenas dentro da função.
+
+console.log(sum(10, 50))
+
+console.log(number1)
