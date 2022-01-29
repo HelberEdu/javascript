2ad2886e192c801f6ae90017386cc64069ef263c
Helber <helber.edu@gmail.com>
2022-01-28 23:22:12 -0300

    seventh day, practicing comparison operators

---
+++ javascript/7. operadoresDeComparação/app.js
@@ -0,0 +1,29 @@
+if (10 === 10){ // === compara não só se os valores são iguais, mas também os tipos
+    console.log('Mesmo tipo')
+}
+
+if (10 != 1){
+    console.log('É diferente')
+}
+
+if (10 > 1){
+    console.log('É maior')
+}
+
+if (10 >= 1){
+    console.log('É maior ou igual')
+}
+
+if (10 <= 10){
+    console.log('É maior ou igual')
+}
+
+/*
+==      Igual
+===     Exatamente igual(valor e tipo)
+!=      Não é igual
+>       Maior
+<       Menor
+>=      Maior ou igual
+<=      Menor ou igual
+*/
