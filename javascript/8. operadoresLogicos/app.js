299bda87d6c43d8dff792b10fd3cd635161e5262
HelberEdu <helber.edu@gmail.com>
2022-01-29 23:49:21 -0300

    eighth day, practicing logical operators

---
+++ javascript/8. operadoresLogicos/app.js
@@ -0,0 +1,30 @@
+// and, or, not
+// &&, ||, !
+
+// var email = 'joao@gmail.com'
+// var password = '124'
+
+// if (email === 'joao@gmail.com' && password === '1234'){
+//     console.log('Usuário autenticado')
+// } else {
+//     console.log('Dados inválidos')
+// }
+
+//////////////////////////////////////
+
+// var age = 23
+// var sex = 'F'
+
+// if (age >= 18 || sex === 'F'){
+//     console.log('Entrada liberada')
+// } else{
+//     console.log('Entrada barrada')
+// }
+
+var sex = 'F'
+
+if(!sex === 'F'){
+    console.log('Entrada liberada')
+} else{
+    console.log('Entrada barrada')
+}
