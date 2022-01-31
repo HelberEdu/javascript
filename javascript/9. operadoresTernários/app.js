03fb47e1f56e8925432bf4c908b735566f83f9c3
HelberEdu <helber.edu@gmail.com>
2022-01-30 22:51:49 -0300

    ninth day, practicing ternary operators

---
+++ javascript/9. operadoresTernários/app.js
@@ -0,0 +1,12 @@
+var age = 18
+var result = age >= 18 ? 'ok' : 'não ok'
+// ? representa o se e : representa o se não
+// condição ? true : false
+// if (age == 18){
+//     result = 'ok'
+// } else {
+//     result = 'não ok'
+// }
+
+
+console.log(result)
