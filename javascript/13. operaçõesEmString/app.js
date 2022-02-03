171275144d1495a242cdcaa2ec5a4bc84ec47adb
HelberEdu <helber.edu@gmail.com>
2022-02-03 12:35:05 -0300

    thirteenth day, practicing string operations

---
+++ javascript/13. operaçõesEmString/app.js
@@ -0,0 +1,29 @@
+// var person = {
+//     age: 23,
+//     height: 7.3,
+//     sayHello: function(){
+//         console.log('Hello')
+//     }
+// }
+
+// person.sayHello()
+
+var text = 'Hello Helber'
+
+// var value = text.length // length é uma propriedade de tamanho que inclue espaços em branco na contagem.
+
+// var value= text[0] // os colchetes acessam o valor na posição simbolizada pelo número, sendo 0 a primeira posição.
+
+// var value = text.replace('o', '@') // método que remove um ou mais caracteres e adiciona outros em seu lugar.
+
+// var value = text.toUpperCase() // método que transforma toda a string em letras maíusculas.
+
+// var value = text.toLowerCase() // métoro que transforma toda a string em letras minúsculas.
+
+// var value = text.trim() // remove o espaçamento do início e do fim da string.
+
+var value = text.split(' ') // método que divide uma string em um array de strings. Basta colocar o caractere entre as áspas simples para dividir os caracteres que estão antes dos que estão depois do caractere no split.
+
+
+console.log(text)
+console.log(value)
