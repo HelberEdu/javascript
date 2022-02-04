8e3e9fb2ecf8bbc53227db0a2cf8814847ce52d8
HelberEdu <helber.edu@gmail.com>
2022-02-03 23:45:19 -0300

    fourteenth day, practicing operations on numbers

---
+++ javascript/app.js
@@ -0,0 +1,10 @@
+var number = 10.3
+
+// var newValue = number.toString() // transforma o número em string
+
+// var newValue = Math.trunc(number) // arredonda p número para baixo.
+// var newValue = Math.ceil(number) // arredonda o número para cima.
+var newValue = Math.round(number) // arredonda para o mais próximo
+
+
+console.log(newValue)
