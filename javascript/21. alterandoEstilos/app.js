3b119e7be04ab0ced85cbb997768a1b37e75f8ec
HelberEdu <helber.edu@gmail.com>
2022-02-12 23:36:54 -0300

    twenty-first day, practicing changing styles

---
+++ javascript/21. alterandoEstilos/app.js
@@ -0,0 +1,8 @@
+// var element = document.querySelector('body')
+var element = document.querySelector('div#app')
+
+element.style.backgroundColor = '#000' // o espaço da propriedade de estilização é trocada pela sintaxe do camelCase
+
+element.style.height = '100px'
+element.style.width = '100px'
+element.style.borderRadius = '20px'
